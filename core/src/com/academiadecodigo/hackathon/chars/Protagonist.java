ddaaa95506e05fc0bd200bbcfa015dd3bf33cbc4
fc43974 <fc43974@alunos.fc.ul.pt>
2020-04-08 18:26:09 +0100

    Minor fixes.
    Area controls about boundaries.

--- core/src/com/academiadecodigo/hackathon/chars/Protagonist.java
+++ core/src/com/academiadecodigo/hackathon/chars/Protagonist.java
@@ -2,4 +2,17 @@ package com.academiadecodigo.hackathon.chars;
 
 public class Protagonist extends Person {
 
+    private int enemiesDefeated;
+
+    public void incrementKillCounter() {
+        enemiesDefeated++;
+    }
+
+    public int getEnemiesDefeated() {
+        return enemiesDefeated;
+    }
+
+    public void setEnemiesDefeated(int enemiesDefeated) {
+        this.enemiesDefeated = enemiesDefeated;
+    }
 }
