b7cc1a3a4c87c6d011b546e18136e3b760368c2a
DavidBorges-2 <borges.david6@gmail.com>
2020-04-09 13:15:25 +0100

    Battle View Graphic Logic

--- core/src/com/academiadecodigo/hackathon/Game.java
+++ core/src/com/academiadecodigo/hackathon/Game.java
@@ -1,10 +1,13 @@
 package com.academiadecodigo.hackathon;
 
+import com.academiadecodigo.hackathon.view.BattleView;
+import com.academiadecodigo.hackathon.view.GameView;
 import com.academiadecodigo.hackathon.visuals.GameSound;
 import com.academiadecodigo.hackathon.visuals.TopDownCamera;
 import com.academiadecodigo.hackathon.world.Coord;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -26,12 +29,14 @@ public class Game extends ApplicationAdapter {
 
 	TopDownCamera topDownCamera;
 	GameSound sound;
+	Screen screen;
 
 	WorldLogic worldLogic;
 	boolean runWorldLogic;
 
 	BattleLogic battleLogic;
 	boolean runBattleLogic;
+
 
 	@Override
 	public void create () {
@@ -62,30 +67,73 @@ public class Game extends ApplicationAdapter {
 		worldLogic.setPlayerPosition(playerPos);
 
 		sound = new GameSound();
-		//sound.gameMusic();
-		sound.battleMusic();
+		sound.gameMusic();
+
+		runWorldLogic = false;
+		runBattleLogic = true;
+
 
 	}
 
 	@Override
 	public void render () {
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		tiledMapRenderer.setView(camera);
-		tiledMapRenderer.render();
+		if(runWorldLogic) {
+			/*
+			Gdx.gl.glClearColor(1, 0, 0, 1);
+			Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			camera.update();
+			tiledMapRenderer.setView(camera);
+			tiledMapRenderer.render();
+
+			sb.begin();
+			sprite.draw(sb);
+			sb.end();
+
+			 */
+			setScreen(new GameView(this, camera,tiledMapRenderer,sprite,sb));
+			screen.render(1);
+			sound.stopBattleMusic();
+			sound.gameMusic();
+
+			/*runWorldLogic = false;
+			runBattleLogic = true;
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			render();*/
+		}
+
+		if(runBattleLogic) {
+			setScreen(new BattleView(this));
+			screen.render(1);
+			sound.stopGameMusic();
+			sound.battleMusic();
+
+			runBattleLogic = false;
+			runWorldLogic = true;
+
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			render();
+		}
 
 
-		sb.begin();
-		sprite.draw(sb);
-		sb.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 	}
 
-
+	public void setScreen(Screen screen) {
+		this.screen = screen;
+	}
 }
---
+++ core/src/com/academiadecodigo/hackathon/view/GameView.java
@@ -0,0 +1,72 @@
+package com.academiadecodigo.hackathon.view;
+
+import com.academiadecodigo.hackathon.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+
+public class GameView implements Screen {
+
+    private final Game game;
+    private OrthographicCamera camera;
+    private TiledMapRenderer tiledMapRenderer;
+    private Sprite sprite;
+    private SpriteBatch sb;
+
+    public GameView(final Game game, OrthographicCamera camera, TiledMapRenderer tiledMapRenderer, Sprite sprite, SpriteBatch sb) {
+        this.game = game;
+        this.camera = camera;
+        this.tiledMapRenderer = tiledMapRenderer;
+        this.sprite = sprite;
+        this.sb = sb;
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        sb.begin();
+        sprite.draw(sb);
+        sb.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
